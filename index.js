c82fce62c232f1d6559d207a3e9937a6d6e6c7d2
hectorfjaenz <hectorfjaenz@gmail.com>
2022-08-31 20:29:55 -0600

    Upload files

---
+++ index.js
@@ -0,0 +1,12 @@
+ScrollReveal().reveal(".title", { duration: 2000, distance: "-50px" });
+ScrollReveal().reveal(".text", { duration: 1500, distance: "-50px" });
+ScrollReveal().reveal(".button", {
+  distance: "100px",
+  duration: 1000,
+  origin: "bottom",
+});
+ScrollReveal().reveal(".character", {
+  duration: 1000,
+  distance: "60px",
+  origin: "right",
+});
